9f0b0378bc8c7e6ee2a6c097ee335468ebc6e4e0
yamanobori_old <yamanobori_old@yahoo.co.jp>
2017-02-19 21:52:31 +0900

    再帰の改善など

--- main.go
+++ main.go
@@ -12,10 +12,11 @@ const (
 	// Piece 駒
 	Piece
 
-	// Control 利き
+	// Control Queenの利き
 	Control
 )
 
+// 方角
 const (
 	north = iota
 	northeast
@@ -34,13 +35,111 @@ type Pos struct {
 	Y int
 }
 
+const (
+	width  = 8
+	height = 8
+)
+
+// Board 盤面表現
+type Board [width][height]int
+
+// 盤面の初期化
+func (p *Board) initialize() {
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			pos := Pos{x, y}
+			p[pos.Y][pos.X] = Empty
+		}
+	}
+}
+
+// 盤面の範囲内か?
+func (p *Board) isInsidePos(pos Pos) bool {
+	return 0 <= pos.X && pos.X < width && 0 <= pos.Y && pos.Y < height
+}
+func (p *Board) canPut(pos Pos) bool {
+	return p.isInsidePos(pos) && p[pos.Y][pos.X] == Empty
+}
+
+func getIncrementNum(dir int) (int, int) {
+	incrementMap := map[int]struct{ x, y int }{
+		north:     {0, -1},
+		northeast: {1, -1},
+		east:      {1, 0},
+		southeast: {1, 1},
+		south:     {0, 1},
+		southwest: {-1, 1},
+		west:      {-1, 0},
+		northwest: {-1, -1},
+	}
+	val := incrementMap[dir]
+	return val.x, val.y
+}
+
+// 駒を置いて,利きを足す
+func (p *Board) putAndAddControl(here Pos) {
+	// その場に置く
+	p[here.Y][here.X] = Piece
+
+	// 8方位に展開する
+	for d := north; d < numOfDir; d++ {
+		pos := here
+		incX, incY := getIncrementNum(d)
+		pos.X += incX
+		pos.Y += incY
+
+		// 利きを伸ばす
+		for p.isInsidePos(pos) {
+			p[pos.Y][pos.X] = Control
+			pos.X += incX
+			pos.Y += incY
+		}
+	}
+}
+
+func solve() []Board {
+	var (
+		board     Board
+		completed []Board
+	)
+	board.initialize()
+	solveRecursive(board, 0, &completed)
+	return completed
+}
+func solveRecursive(board Board, y int, completed *[]Board) {
+	if y == width {
+		// もうこれ以上置けないので完成
+		*completed = append(*completed, board)
+		return
+	}
+	for x := 0; x < width; x++ {
+		if board.canPut(Pos{x, y}) {
+			copyBoad := board
+			copyBoad.putAndAddControl(Pos{x, y})
+			// 次の行を調べる
+			solveRecursive(copyBoad, y+1, completed)
+		}
+	}
+	return
+}
+
+func main() {
+	completed := solve()
+	for _, board := range completed {
+		fmt.Println(board.collectPosOfPiece())
+		fmt.Print(&board)
+		fmt.Println("###############")
+	}
+	fmt.Println(len(completed))
+}
+
+/*
+ * 主にデバッグ用
+ */
 // Stringer
 func (o Pos) String() string {
 	return fmt.Sprintf("(%v,%v)", o.X, o.Y)
 }
-
-// Board 盤面表現
-type Board [8][8]int
 
 // Stringer
 func (p *Board) String() string {
@@ -62,9 +161,10 @@ func (p *Board) String() string {
 	}
 	return buffer.String()
 }
-func (p *Board) piecesPos() (poses []Pos) {
-	for x := 0; x < 8; x++ {
-		for y := 0; y < 8; y++ {
+
+func (p *Board) collectPosOfPiece() (poses []Pos) {
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
 			pos := Pos{x, y}
 			if p[pos.Y][pos.X] == Piece {
 				poses = append(poses, pos)
@@ -72,85 +172,4 @@ func (p *Board) piecesPos() (poses []Pos) {
 		}
 	}
 	return
-}
-func (p *Board) initialize() {
-	for x := 0; x < 8; x++ {
-		for y := 0; y < 8; y++ {
-			pos := Pos{x, y}
-			p[pos.Y][pos.X] = Empty
-		}
-	}
-}
-func (p *Board) inboard(pos Pos) bool {
-	return 0 <= pos.X && pos.X <= 7 && 0 <= pos.Y && pos.Y <= 7
-}
-func (p *Board) isPutable(pos Pos) bool {
-	return p.inboard(pos) && p[pos.Y][pos.X] == Empty
-}
-func getIncVal(dir int) (int, int) {
-	switch dir {
-	case north:
-		return 0, -1
-	case northeast:
-		return 1, -1
-	case east:
-		return 1, 0
-	case southeast:
-		return 1, 1
-	case south:
-		return 0, 1
-	case southwest:
-		return -1, 1
-	case west:
-		return -1, 0
-	case northwest:
-		return -1, -1
-	default:
-		// must not be reach
-		return 0, 0
-	}
-}
-func (p *Board) put(center Pos) {
-	p[center.Y][center.X] = Piece
-	for d := north; d < numOfDir; d++ {
-		pos := center
-		incX, incY := getIncVal(d)
-		pos.X += incX
-		pos.Y += incY
-		for p.inboard(pos) {
-			p[pos.Y][pos.X] = Control
-			pos.X += incX
-			pos.Y += incY
-		}
-	}
-}
-
-func solve() (complete []Board) {
-	var board Board
-	board.initialize()
-	solveRecursive(board, 0, &complete)
-	return
-}
-func solveRecursive(board Board, currentY int, complete *[]Board) {
-	for x := 0; x < 8; x++ {
-		if board.isPutable(Pos{x, currentY}) {
-			copyBoad := board
-			copyBoad.put(Pos{x, currentY})
-			if currentY == 7 {
-				*complete = append(*complete, copyBoad)
-			} else {
-				solveRecursive(copyBoad, currentY+1, complete)
-			}
-		}
-	}
-}
-
-func main() {
-	complete := solve()
-	for _, board := range complete {
-		fmt.Println(board.piecesPos())
-		fmt.Print(&board)
-		fmt.Println("###############")
-	}
-	fmt.Println(len(complete))
 }
